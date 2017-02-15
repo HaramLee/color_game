7e611d4c59e864ac10dd9b46303b44926751f6b8
DESKTOP-P7VSB05\Haram <haram.lee@hotmail.ca>
2017-02-15 12:02:44 -0800

    Added click event listener for color boxes to see if the user choose the correct color matches

--- color_game/colorGame.js
+++ color_game/colorGame.js
@@ -1,3 +1,4 @@
+//Sets of color
 var colors = ["rgb(255,0,0)",
 			  "rgb(255,255,0)",
 			  "rgb(0,255,0)",
@@ -7,6 +8,27 @@ var colors = ["rgb(255,0,0)",
 
 var squares = document.querySelectorAll(".square");
 
+//Letting rgb(0,255,255) be the selector answer.
+var pickedColor = colors[3];
+
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
 for(var i = 0; i < squares.length; i++){
+	//Add initial colors to squares
 	squares[i].style.background = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click",function(){
+		//grab color of clicked square
+		var clickedColor = this.style.background;
+		//compare color to picked color
+		if(clickedColor === pickedColor){
+			alert("Correct");
+		} else {
+			alert("Incorrect");
+		}
+
+	});
 }
