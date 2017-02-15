09a7cc326b65e1235b8a887c7ff0d452067cb02c
DESKTOP-P7VSB05\Haram <haram.lee@hotmail.ca>
2017-02-15 12:35:26 -0800

    Made it so that instead of using static color variable, we would generate random RGB color

--- color_game/colorGame.js
+++ color_game/colorGame.js
@@ -1,14 +1,10 @@
 //Sets of color
-var colors = ["rgb(255, 0, 0)",
-			  "rgb(255, 255, 0)",
-			  "rgb(0, 255, 0)",
-			  "rgb(0, 255, 255)",
-			  "rgb(0, 0, 255)",
-			  "rgb(255, 0, 255)"]
+var colors = generateColors(6);
 
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 //Letting rgb(0,255,255) be the selector answer.
 var pickedColor = pickRandom();
@@ -27,6 +23,7 @@ for(var i = 0; i < squares.length; i++){
 		if(clickedColor === pickedColor){
 			messageDisplay.textContent = "Correct";
 			changeColor(clickedColor);
+
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again";
@@ -41,8 +38,31 @@ function changeColor(color){
 		//change each color of every box to given color
 		squares[i].style.background = color;
 	}
+	//change background of h1 as well
+	h1.style.background = color;
 }
 
 function pickRandom(){
 	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function generateColors(n){
+	//Make an array
+	var tmpArr = [];
+	//add colors to the array
+	for(var i =0; i < n;i++){
+		//assign random color to the array
+		tmpArr.push(randomColor());
+	}
+
+	return tmpArr;
+}
+
+function randomColor(){
+	//Make three variable red, green, blue with num (0-255)
+	var red = Math.floor(Math.random() * 256);
+	var green = Math.floor(Math.random() * 256);
+	var blue = Math.floor(Math.random() * 256);
+
+	return "rgb("+red+", "+green+", "+blue+")";
 }
