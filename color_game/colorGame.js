18166de5976c64e9d49ccd40f15fc1bef6619c38
DESKTOP-P7VSB05\Haram <haram.lee@hotmail.ca>
2017-02-15 12:22:12 -0800

    Added feature to select random color from the array instead of using static value. When user selects the correct color, all square will changes its color to match the correct color.

--- color_game/colorGame.js
+++ color_game/colorGame.js
@@ -1,17 +1,17 @@
 //Sets of color
-var colors = ["rgb(255,0,0)",
-			  "rgb(255,255,0)",
-			  "rgb(0,255,0)",
-			  "rgb(0,255,255)",
-			  "rgb(0,0,255)",
-			  "rgb(255,0,255)"]
+var colors = ["rgb(255, 0, 0)",
+			  "rgb(255, 255, 0)",
+			  "rgb(0, 255, 0)",
+			  "rgb(0, 255, 255)",
+			  "rgb(0, 0, 255)",
+			  "rgb(255, 0, 255)"]
 
 var squares = document.querySelectorAll(".square");
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 //Letting rgb(0,255,255) be the selector answer.
-var pickedColor = colors[3];
-
-var colorDisplay = document.getElementById("colorDisplay");
+var pickedColor = pickRandom();
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,10 +25,24 @@ for(var i = 0; i < squares.length; i++){
 		var clickedColor = this.style.background;
 		//compare color to picked color
 		if(clickedColor === pickedColor){
-			alert("Correct");
+			messageDisplay.textContent = "Correct";
+			changeColor(clickedColor);
 		} else {
-			alert("Incorrect");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 
 	});
+}
+
+function changeColor(color){
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++){
+		//change each color of every box to given color
+		squares[i].style.background = color;
+	}
+}
+
+function pickRandom(){
+	return colors[Math.floor(Math.random() * colors.length)];
 }
